992f4463d48c0855cc0f3b15288d32108cfbf120
101 <tmddnjs001133@dsm.hs.kr>
2024-09-03 18:59:48 +0900

    init: change the main class name

--- src/main/kotlin/com/example/melancholic/Application.kt
+++ src/main/kotlin/com/example/melancholic/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MelancholicApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<MelancholicApplication>(*args)
+	runApplication<Application>(*args)
 }
